1cd0011d17f5e8d6798fb016c17cf9ba8ff91760
Brian McCann <bmccann36@gmail.com>
2017-09-22 17:31:58 -0400

    day1 setup

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ const app = express();
 const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+
+
+app.use(morgan('dev'))
+
+const server = app.listen(3000, function(){
+  console.log('listening on port 3000')
+})
+
+app.get('/', function(req, res){
+  res.send('you got the home page')
+})
