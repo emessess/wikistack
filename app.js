25093d9d8d496428439ad0e822cb519296a0635f
Brian McCann <bmccann36@gmail.com>
2017-09-25 11:11:50 -0400

    db is synced created two tables

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express();
 const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+var models = require('./models');
+
 
 app.use(express.static('public'));
 
@@ -20,9 +20,25 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
 app.use(bodyParser.json()); // would be for AJAX requests
 
-const server = app.listen(3000, function(){
-  console.log('listening on port 3000');
-});
+
+models.db.sync({})
+.then(function () {
+    app.listen(3000, function () {
+        console.log('Server is listening on andre 3000!');
+    });
+})
+.catch(console.error);
+
+// models.User.sync({})
+// .then(function () {
+//     return models.Page.sync({})
+// })
+// .then(function () {
+//     app.listen(3000, function () {
+//         console.log('Server is listening on port 3000!');
+//     });
+// })
+// .catch(console.error);
 
 app.get('/', function(req, res){
   res.render('index');
