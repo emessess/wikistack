668f12df984fd28870d65b266ed8acb4745d4ff1
Matthew Smith <mattsheridansmith@gmail.com>
2017-09-25 11:29:23 -0400

    model settings

--- app.js
+++ app.js
@@ -14,31 +14,17 @@ app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true});
 
-
 app.use(morgan('dev'));
-
 app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
 app.use(bodyParser.json()); // would be for AJAX requests
 
-
 models.db.sync({})
-.then(function () {
+  .then(function () {
     app.listen(3000, function () {
-        console.log('Server is listening on andre 3000!');
+      console.log('Server is listening on andre 3000!');
     });
-})
-.catch(console.error);
-
-// models.User.sync({})
-// .then(function () {
-//     return models.Page.sync({})
-// })
-// .then(function () {
-//     app.listen(3000, function () {
-//         console.log('Server is listening on port 3000!');
-//     });
-// })
-// .catch(console.error);
+  })
+  .catch(console.error);
 
 app.get('/', function(req, res){
   res.render('index');
