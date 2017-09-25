d5c5a3672b5a5306d4edc0a6946ee01d4a5cfbdd
Matthew Smith <mattsheridansmith@gmail.com>
2017-09-25 10:28:57 -0400

    boilerplate

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const app = express();
 const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
+
+app.use(express.static('public'));
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
 
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
 
 const server = app.listen(3000, function(){
-  console.log('listening on port 3000')
-})
+  console.log('listening on port 3000');
+});
 
 app.get('/', function(req, res){
-  res.send('you got the home page')
-})
+  res.render('index');
+});
