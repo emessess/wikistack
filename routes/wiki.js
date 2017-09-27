01f9e157577f6564033190aa9c13ee73238a5188
Matthew Smith <mattsheridansmith@gmail.com>
2017-09-26 22:59:41 -0400

    stuff

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,21 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const models = require('../models');
-const Page = models.page;
-const User = models.user;
+const Page = models.Page;
+const User = models.User;
 
 router.get('/', (req, res, next) => {
   res.redirect('/');
 });
 
 router.post('/', (req, res, next) => {
-  // res.json(req.body);
-  const page = Page.build({
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  page.save()
+  const newPage = Page.build(req.body);
+  newPage.save().then(() => res.redirect(newPage.route));
   
 
 });
